297506880d096a8644ad8c44fa621a82da1d4caf
Alexdoru <57050655+alexdoru@users.noreply.github.com>
2022-01-12 18:41:58 +0100

    fixed indentation

--- src/main/java/fr/alexdoru/sidebarmod/gui/screen/GuiScreenSidebar.java
+++ src/main/java/fr/alexdoru/sidebarmod/gui/screen/GuiScreenSidebar.java
@@ -9,48 +9,45 @@ import net.minecraft.scoreboard.ScoreObjective;
 import java.io.IOException;
 
 public class GuiScreenSidebar extends GuiScreen {
-  protected SidebarMod mod;
-  
-  protected GuiSidebar sidebar;
-  
-  private boolean dragging;
-  
-  private int lastX;
-  
-  private int lastY;
-  
-  public GuiScreenSidebar(SidebarMod mod) {
-    this.mod = mod;
-    this.sidebar = mod.getSidebarGui();
-  }
-  
-  public void drawScreen(int mouseX, int mouseY, float partialTicks) {
-    super.drawScreen(mouseX, mouseY, partialTicks);
-    if (this.mc.thePlayer != null) {
-      ScoreObjective scoreObjective = this.mc.thePlayer.getWorldScoreboard().getObjectiveInDisplaySlot(1);
-      if (scoreObjective != null)
-        this.sidebar.drawSidebar(scoreObjective, new ScaledResolution(this.mc)); 
-    } 
-    if (this.dragging) {
-      this.sidebar.offsetX += mouseX - this.lastX;
-      this.sidebar.offsetY += mouseY - this.lastY;
-    } 
-    this.lastX = mouseX;
-    this.lastY = mouseY;
-  }
-  
-  protected void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException {
-    super.mouseClicked(mouseX, mouseY, mouseButton);
-    if (this.sidebar.contains(mouseX, mouseY))
-      this.dragging = true; 
-  }
-  
-  protected void mouseReleased(int mouseX, int mouseY, int state) {
-    super.mouseReleased(mouseX, mouseY, state);
-    this.dragging = false;
-  }
-  
-  public void onGuiClosed() {
-    this.mod.saveConfig();
-  }
+
+    protected SidebarMod mod;
+    protected GuiSidebar sidebar;
+    private boolean dragging;
+    private int lastX;
+    private int lastY;
+
+    public GuiScreenSidebar(SidebarMod mod) {
+        this.mod = mod;
+        this.sidebar = mod.getSidebarGui();
+    }
+
+    public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+        super.drawScreen(mouseX, mouseY, partialTicks);
+        if (this.mc.thePlayer != null) {
+            ScoreObjective scoreObjective = this.mc.thePlayer.getWorldScoreboard().getObjectiveInDisplaySlot(1);
+            if (scoreObjective != null)
+                this.sidebar.drawSidebar(scoreObjective, new ScaledResolution(this.mc));
+        }
+        if (this.dragging) {
+            this.sidebar.offsetX += mouseX - this.lastX;
+            this.sidebar.offsetY += mouseY - this.lastY;
+        }
+        this.lastX = mouseX;
+        this.lastY = mouseY;
+    }
+
+    protected void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException {
+        super.mouseClicked(mouseX, mouseY, mouseButton);
+        if (this.sidebar.contains(mouseX, mouseY))
+            this.dragging = true;
+    }
+
+    protected void mouseReleased(int mouseX, int mouseY, int state) {
+        super.mouseReleased(mouseX, mouseY, state);
+        this.dragging = false;
+    }
+
+    public void onGuiClosed() {
+        this.mod.saveConfig();
+    }
 }
