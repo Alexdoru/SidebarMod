b4c2f62d13684fd94476b0e884fe226fcbcbc765
Alexdoru <57050655+alexdoru@users.noreply.github.com>
2022-01-12 21:43:10 +0100

    oui

--- src/main/java/fr/alexdoru/sidebarmod/gui/GuiSidebar.java
+++ src/main/java/fr/alexdoru/sidebarmod/gui/GuiSidebar.java
@@ -4,7 +4,10 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.ScaledResolution;
-import net.minecraft.scoreboard.*;
+import net.minecraft.scoreboard.Score;
+import net.minecraft.scoreboard.ScoreObjective;
+import net.minecraft.scoreboard.ScorePlayerTeam;
+import net.minecraft.scoreboard.Scoreboard;
 import net.minecraft.util.EnumChatFormatting;
 import org.lwjgl.opengl.GL11;
 
@@ -34,19 +37,20 @@ public class GuiSidebar extends Gui {
         float mscale = this.scale - 1.0F;
         float minX = this.sidebarX - this.sidebarWidth * mscale;
         float maxX = minX + this.sidebarWidth * this.scale;
-        float maxY = (this.sidebarY + (this.sidebarHeight / 2)) * mscale;
+        float maxY = (this.sidebarY + ((float) this.sidebarHeight / 2)) * mscale;
         float minY = maxY - this.sidebarHeight * this.scale;
         return (mouseX > minX && mouseX < maxX && mouseY > minY - this.fr.FONT_HEIGHT * this.scale && mouseY < maxY);
     }
 
-    public void drawSidebar(ScoreObjective sidebar, ScaledResolution res) {
-        if (!this.enabled)
+    public void drawSidebar(ScoreObjective scoreObjective, ScaledResolution scaledResolution) {
+        if (!this.enabled) {
             return;
+        }
         FontRenderer fr = (Minecraft.getMinecraft()).fontRendererObj;
-        Scoreboard scoreboard = sidebar.getScoreboard();
+        Scoreboard scoreboard = scoreObjective.getScoreboard();
         List<Score> scores = new ArrayList<>();
-        this.sidebarWidth = fr.getStringWidth(sidebar.getDisplayName());
-        for (Score score : scoreboard.getSortedScores(sidebar)) {
+        this.sidebarWidth = fr.getStringWidth(scoreObjective.getDisplayName());
+        for (Score score : scoreboard.getSortedScores(scoreObjective)) {
             String name = score.getPlayerName();
             if (scores.size() < 15 && name != null && !name.startsWith("#")) {
                 ScorePlayerTeam scorePlayerTeam = scoreboard.getPlayersTeam(name);
@@ -57,8 +61,8 @@ public class GuiSidebar extends Gui {
             }
         }
         this.sidebarHeight = scores.size() * fr.FONT_HEIGHT;
-        this.sidebarX = res.getScaledWidth() - this.sidebarWidth - 3 + this.offsetX;
-        this.sidebarY = res.getScaledHeight() / 2 + this.sidebarHeight / 3 + this.offsetY;
+        this.sidebarX = scaledResolution.getScaledWidth() - this.sidebarWidth - 3 + this.offsetX;
+        this.sidebarY = scaledResolution.getScaledHeight() / 2 + this.sidebarHeight / 3 + this.offsetY;
         int scalePointX = this.sidebarX + this.sidebarWidth;
         int scalePointY = this.sidebarY - this.sidebarHeight / 2;
         float mscale = this.scale - 1.0F;
@@ -75,13 +79,13 @@ public class GuiSidebar extends Gui {
             int scoreX = this.sidebarX + this.sidebarWidth + 1;
             int scoreY = this.sidebarY - index * fr.FONT_HEIGHT;
             drawRect(this.sidebarX - 2, scoreY, scoreX, scoreY + fr.FONT_HEIGHT, getColor(false));
-            drawString(s1, this.sidebarX, scoreY, 553648127);
-            drawString(s2, scoreX - fr.getStringWidth(s2), scoreY, 553648127);
+            drawString(s1, this.sidebarX, scoreY);
+            drawString(s2, scoreX - fr.getStringWidth(s2), scoreY);
             if (index == scores.size()) {
-                String s3 = sidebar.getDisplayName();
+                String s3 = scoreObjective.getDisplayName();
                 drawRect(this.sidebarX - 2, scoreY - fr.FONT_HEIGHT - 1, scoreX, scoreY - 1, getColor(true));
                 drawRect(this.sidebarX - 2, scoreY - 1, scoreX, scoreY, getColor(false));
-                drawString(s3, this.sidebarX + (this.sidebarWidth - fr.getStringWidth(s3)) / 2, scoreY - fr.FONT_HEIGHT, 553648127);
+                drawString(s3, this.sidebarX + (this.sidebarWidth - fr.getStringWidth(s3)) / 2, scoreY - fr.FONT_HEIGHT);
             }
         }
         GL11.glScalef(1.0F / this.scale, 1.0F / this.scale, 1.0F);
@@ -104,11 +108,11 @@ public class GuiSidebar extends Gui {
         return a << 24 | r << 16 | g << 8 | b;
     }
 
-    private void drawString(String str, int x, int y, int color) {
+    private void drawString(String str, int x, int y) {
         if (this.shadow) {
-            this.fr.drawStringWithShadow(str, x, y, color);
+            this.fr.drawStringWithShadow(str, x, y, -1);
         } else {
-            this.fr.drawString(str, x, y, color);
+            this.fr.drawString(str, x, y, -1);
         }
     }
 }
