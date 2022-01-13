4e8b500312be7c12a05c3a86987413c35f01905a
Alexdoru <alex.dolley@free.fr>
2022-01-13 15:37:24 +0100

    e

--- src/main/java/fr/alexdoru/sidebarmod/asm/ClassTransformer.java
+++ src/main/java/fr/alexdoru/sidebarmod/asm/ClassTransformer.java
@@ -5,10 +5,10 @@ import net.minecraft.launchwrapper.IClassTransformer;
 public class ClassTransformer implements IClassTransformer {
     @Override
     public byte[] transform(String name, String transformedName, byte[] basicClass) {
-        if(basicClass == null){
+        if (basicClass == null) {
             return null;
         }
-        if(transformedName.equals("net.minecraft.client.gui.GuiIngame")){
+        if (transformedName.equals("net.minecraft.client.gui.GuiIngame")) {
             return new GuiIngameTransformer().transform(name, transformedName, basicClass);
         }
         return basicClass;
